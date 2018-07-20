0c2bbcffef934989a94f8165732202d5ae6f77f4
Manel Matougui <m.matougui@gmail.com>
2018-07-20 15:47:10 -0700

    first loginView Scene

---
+++ On The Map/On The Map/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Manel matougui on 7/19/18.
+//  Copyright © 2018 udacity. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: Outletss
+    
+    @IBOutlet weak var UdacityImageView: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var debugTextLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
