ef50d617bc970fc97f4b77fa2baf09afccdd8f26
Manel Matougui <m.matougui@gmail.com>
2018-07-28 23:32:39 -0700

    fix tableView (row open link)

--- On The Map/On The Map/TableTabViewController.swift
+++ On The Map/On The Map/TableTabViewController.swift
@@ -11,21 +11,13 @@ import UIKit
 class TableTabViewController: UITableViewController {
     
     // Properties
-    var students = [Student]()
+    var locations = [[String: Any]]()
     
     // Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        initStudents()
-    }
+        locations = hardCodedLocationData()
 
-    func initStudents() {
-        students.append(Student(name: "Malak Matougui", url: "http://malak.matougui.com"))
-        students.append(Student(name: "Malak Matougui", url: "http://malak.matougui.com"))
-        students.append(Student(name: "Malak Matougui", url: "http://malak.matougui.com"))
-        //let student = students[0]
-        //let name = student.Name
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -35,14 +27,23 @@ class TableTabViewController: UITableViewController {
     // UITableViewController
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return students.count
+        return locations.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "Student Cell")!
-        let student = students[(indexPath as NSIndexPath).row]
-        cell.textLabel!.text = student.Name
-        cell.detailTextLabel!.text = student.Url
+        let location = locations[(indexPath as NSIndexPath).row]
+        cell.textLabel!.text = location["firstName"] as? String
+        cell.detailTextLabel!.text = location["mediaURL"] as? String
+//        let app = UIApplication.shared
+//        app.openURL(URL(string: location["mediaURL"] as! String)!)
+
        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let location = locations[(indexPath as NSIndexPath).row]
+        let app = UIApplication.shared
+        app.openURL(URL(string: location["mediaURL"] as! String)!)
+
     }
 
     
@@ -83,6 +84,55 @@ class TableTabViewController: UITableViewController {
 //            print(String(data: newData!, encoding: .utf8)!)
         }
         task.resume()
+    }
+    func hardCodedLocationData() -> [[String : Any]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-02-24T22:30:54.442Z",
+                "firstName" : "Jason",
+                "lastName" : "Schatz",
+                "latitude" : 37.7617,
+                "longitude" : -122.4216,
+                "mapString" : "18th and Valencia, San Francisco, CA",
+                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
+                "objectId" : "hiz0vOTmrL",
+                "uniqueKey" : 2362758535,
+                "updatedAt" : "2015-03-10T17:20:31.828Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
     }
     
 }
