78b93d5d664da32de6e1fd73c44eb0cb55a91021
Manel Matougui <m.matougui@gmail.com>
2018-08-09 23:25:53 -0700

    add StudentInformation.swift

---
+++ On The Map/On The Map/StudentInformation.swift
@@ -0,0 +1,20 @@
+//
+//  StudentInformation.swift
+//  On The Map
+//
+//  Created by Manel matougui on 8/9/18.
+//  Copyright © 2018 udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+struct StudentInformation {
+    static var FirstName = ""
+    static var LastName = ""
+    static var MapString = ""
+    static var MediaURL = ""
+    static var Latitude: Double = 0
+    static var Longitude: Double = 0
+    
+}
